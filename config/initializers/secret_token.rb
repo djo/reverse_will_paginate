d06afbcf530ac90325f958f3f46b0ec4e6b3c9fe
Andrew Djoga <andrew.djoga@gmail.com>
2011-12-26 13:28:12 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReverseWillPaginate::Application.config.secret_token = '973030492af3561d16f5c21f08e69f70a083b90a934c1836592a6660f59117471ab09885efe6945bceb9548c150ef3ca8cec8b76c4fba708dc7dd63c4a452ea2'
